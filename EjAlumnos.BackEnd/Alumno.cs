0d61a7b16b244ff23317dacda50a56ebd839220a
Sergio Algorry <40408779+Sergio-Algorry@users.noreply.github.com>
2023-08-30 10:11:29 -0300

    Agregar archivos de proyecto.

---
+++ EjAlumnos.BackEnd/Alumno.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjAlumnos.BackEnd
+{
+    public class Alumno
+    {
+        public string DNI { get; set; }
+
+	}
+}
---
+++ EjAlumnos.FrontEnd/Form1.cs
@@ -0,0 +1,31 @@
+﻿using EjAlumnos.BackEnd;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EjAlumnos.FrontEnd
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btAceptar_Click(object sender, EventArgs e)
+        {
+            //Instaciado el objeto objAlumno de la clase Alumno e inicializado dicho objeto
+            Alumno objAlumno = new Alumno();
+
+            //objAlumno.DNI = "1587458";
+
+            lblSalida.Text = objAlumno.DNI;
+        }
+    }
+}
